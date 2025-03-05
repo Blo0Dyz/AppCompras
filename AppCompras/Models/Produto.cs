63e0351113b6f065e26bd7ce0c1dd0c704b4623d
Marcio <m.antonio.cs00@gmail.com git config --global user.email m.antonio.cs00@gmail.com>
2025-03-05 09:58:00 -0300

    Construção classe SqlLite

--- AppCompras/Models/Produto.cs
+++ AppCompras/Models/Produto.cs
@@ -7,7 +7,15 @@ using SQLite;
 
 namespace AppCompras.Models
 {
-    internal class Produto
+    public class Produto
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public double Quantidade { get; set; }
+
+        public double Preco { get; set; }
     }
 }
